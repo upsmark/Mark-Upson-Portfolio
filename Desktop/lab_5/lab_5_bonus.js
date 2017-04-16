55c051295922e849c0d7895fec2841dbe56950b5
Mark Upson <markaupson@gmail.com>
2017-04-16 15:48:20 -0400

    lab_5_bonus

---
+++ Desktop/lab_5/lab_5_bonus.js
@@ -0,0 +1,24 @@
+//This is the first part of lab 5.
+
+var temperatureFahrenheit = 98.6;
+
+var temperatureACelsius = temperatureFahrenheit - 32;
+
+var temperatureCelsius = temperatureACelsius * (5/9);
+  console.log(temperatureCelsius);
+
+  console.log("The temp in Celsius is "+temperatureCelsius + " The temp in Fahrenheit is "+temperatureFahrenheit);
+
+
+
+//Below is the bonus for lab 5.
+
+var temperatureCelsius = 37;
+
+var temperatureBFahrenheit = temperatureCelsius * (9/5);
+
+var temperatureFahrenheit = temperatureBFahrenheit + 32;
+
+console.log(temperatureFahrenheit);
+
+console.log("This is the temp in Fahrenheit "+temperatureFahrenheit);
